58d12dc063dbb19e06e1a8770c50803aa7ca4f64
koeunyeon <koeunyeon@gmail.com>
2021-11-03 16:30:37 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask import url_for # (1)
 
 app = Flask(__name__)
 
 
-@app.route("/helloworld/")
+@app.route("/hello_world/")
 def helloworld(): 
     return "hello world"
+
+@app.route("/url_for/")  #(2)  
+def sample_url_for():
+    url = url_for("helloworld")
+    return url
